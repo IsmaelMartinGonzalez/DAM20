1d8aecae5ad5e150ad3eece79427dc5cbd28e318
IsmaelMartinGonzalez <fmartin@nigul.cide.es>
2020-12-15 11:10:32 +0100

    Comenzamos actividad de calculo entre dias

---
+++ Programacion/Actividades/src/EjerciciosClase/Objetos/calculoDias/TestDiasEntreDias.java
@@ -0,0 +1,15 @@
+package EjerciciosClase.Objetos.calculoDias;
+
+/**
+ * Project name: DAM20/EjerciciosClase.Objetos.calculoDias
+ * Filename:
+ * Created:  15/12/2020 / 10:43
+ * Description:
+ * Revision:
+ *
+ * @Author: Ismael - fmartin@nigul.cide.es
+ * @Version:
+ */
+public class TestDiasEntreDias {
+    DiesEntreDates_ismael_martin d1 = new DiesEntreDates_ismael_martin();
+}
--- Programacion/Actividades/src/EjerciciosClase/Objetos/coche/TestCoche_Ismael_Martin.java
+++ Programacion/Actividades/src/EjerciciosClase/Objetos/coche/TestCoche_Ismael_Martin.java
@@ -10,23 +10,24 @@ package EjerciciosClase.Objetos.coche;
  */
 /**------------------------------------------------------------------------------------------*/
 public class TestCoche_Ismael_Martin {
-
     /**
      * Desde nuestro mmain llamamos al menu para que comienze nuestro programa a trabajar
      */
-    public static void main(String[] args) {
-        Coche_Ismael_Martin c1= new Coche_Ismael_Martin("Ferrari","G1",TipusCanvi.CanviManual);
+    public static void test() {
+        CotxeSegonaPart_ismael_martin c1= new CotxeSegonaPart_ismael_martin("Ferrari","G1",TipusCanvi.CanviManual,true);
         try {
-            System.out.println(c1.getRevolucions());
-            System.out.println(c1.comprovaMotor());
+            c1.frenar();
+            c1.acelerar();
             c1.arrancarMotor();
-            c1.arrancarMotor();
-            System.out.println(c1.comprovaMotor());
-            System.out.println(c1.getRevolucions());
-            c1.aturarMotor();
-            System.out.println(c1.comprovaMotor());
+            c1.acelerar();
+            System.out.println("Velocidad: "+c1.getVelocidad()+"Km/h");
+            c1.quitarCapota();
+            c1.quitarCapota();
+            c1.frenar();
+            c1.frenar();
         }catch (Exception e){
             System.out.println(e.getMessage());
         }
     }
+
 }
