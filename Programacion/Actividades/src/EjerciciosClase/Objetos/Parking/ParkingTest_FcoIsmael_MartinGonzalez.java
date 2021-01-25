9a166ab0f29d58703d75a7c4690a91c98fce5176
IsmaelMartinGonzalez <fmartin@nigul.cide.es>
2021-01-25 13:04:27 +0100

    Actividad parking casi terminada

--- Programacion/Actividades/src/EjerciciosClase/Objetos/Parking/ParkingTest_FcoIsmael_MartinGonzalez.java
+++ Programacion/Actividades/src/EjerciciosClase/Objetos/Parking/ParkingTest_FcoIsmael_MartinGonzalez.java
@@ -12,12 +12,13 @@ import java.util.Scanner;
  * @Author: Ismael - fmartin@nigul.cide.es
  * @Version:
  */
-public class Test {
+public class ParkingTest_FcoIsmael_MartinGonzalez {
     //Hay que crear una estructura de la bandera para generar un menu que permita llamar a los metodos de la clase Parking
     public static void main(String[] args) throws Exception {
         Scanner sc=new Scanner(System.in);
         String user;
-        Parking p=new Parking(10,10);
+        Parking_FcoIsmael_MartinGonzalez p=new Parking_FcoIsmael_MartinGonzalez(50,50);
+
         try{
             p.llegirMatricules(args[0]);
         }catch (Exception e){
@@ -25,13 +26,15 @@ public class Test {
         }
        boolean exit=false;
         while (!exit){
-            System.out.println("1.Omplir parking a partir de fitxer");
+            System.out.println("----------------------\n"
+                    +"1.Omplir parking a partir de fitxer");
             System.out.println("2.Entrar coche");
             System.out.println("3.Entrar coche Discapacitado");
             System.out.println("4.Salir Coche");
             System.out.println("5.Salir Coche Discapacitado");
             System.out.println("6.Guardar Matricula");
-            System.out.println("7.Salir");
+            System.out.println("7.Salir\n"+
+                    "----------------------");
             System.out.print("Opcion: ");
             int opcion=sc.nextInt();
             try {
@@ -61,12 +64,13 @@ public class Test {
                     }
                     case 6->{
                         System.out.print("Path: ");
-                        String m=sc.next();
-                        p.guardarMatricula(m);
+                        String path=sc.next();
+                        p.guardarMatricula(path);
                     }
                     case 7->{
                         exit=true;
                     }
+                    default -> System.out.println("Error.Selecciona una opción entre 1 y 7");
                 }
             }catch (Exception e){
                 System.out.println(e);
@@ -74,3 +78,45 @@ public class Test {
         }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Programacion/Actividades/src/EjerciciosClase/Objetos/Parking/Parking_FcoIsmael_MartinGonzalez.java
+++ Programacion/Actividades/src/EjerciciosClase/Objetos/Parking/Parking_FcoIsmael_MartinGonzalez.java
@@ -13,10 +13,9 @@ import java.util.*;
  * @Author: Ismael - fmartin@nigul.cide.es
  * @Version:
  */
-public class Parking {
-        //Attriubutes
+public class Parking_FcoIsmael_MartinGonzalez {
+    //Attriubutes
     public enum TipusPlacesParking{Discapacitat,No_Discapacitat}
-    private Scanner sc=new Scanner(System.in);
     private BufferedWriter bw;
     private BufferedReader br;
     private ArrayList<String> matriculas=new ArrayList<>();
@@ -26,44 +25,37 @@ public class Parking {
     private int plazasOcupadas;
 
     //Builder
-    public Parking(int places_no_discapacitats, int places_discapacitats) {
+    public Parking_FcoIsmael_MartinGonzalez(int places_no_discapacitats, int places_discapacitats) {
         this.plazasD=new String[places_discapacitats];
         this.plazas=new String[places_no_discapacitats];
         inicPlazas();
     }
 
     //Getters/Setters
-    private ArrayList<String> getMatriculas() {
-        return matriculas;
-    }
     private String[] getPlazasD() {
         return plazasD;
-    }
-    private String[] getPlazas() {
-        return plazas;
-    }
-    private int getPlazasOcupadasD() {
-        return plazasOcupadasD;
-    }
-    private int getPlazasOcupadas() {
-        return plazasOcupadas;
     }
 
     //Other Methods
     /**Este metodo se encarga de leer y comprobar las matriculas que hay en un archivo de texto plano
+     * Tambien se encargara de llenar el parking en función de las matriculas leidas
      * En caso de no estar bien formado el path, saltara una excepción
      * Si la matricula no esta bien formada saltara una excepción*/
     public void llegirMatricules(String path) throws Exception{
         String linea;
         try {
+            //Comprovamos si la ruta es correcta
             br=new BufferedReader(new FileReader(path));
-            linea=br.readLine();
         }catch (Exception e){
             throw new Exception("ALERTA =====> Fitxer incorrecte o inexistent.");
         }
+        linea=br.readLine();
+        //Generamos un bucle que ira leyendo las lineas del archivo hasta encontrar una linea vacia
         while (linea!=null) {
+            /*Por cada linea, comprobamos que este bien formado según nuestro patron[0-9]{4}[A-Z]{3}, en caso de un error
+             * el programa lanzara una excepción*/
             if (linea.length()==7){
-                char[] chars= linea.toCharArray();
+                char[] chars=linea.toCharArray();
                 for (int i = 0; i < chars.length-3; i++) {
                     if (chars[i]>=48&&chars[i]<=57){
                     }else {
@@ -83,6 +75,8 @@ public class Parking {
                 linea = br.readLine();
             }
         br.close();
+        /*Por ultimo recorremos nuestro array de matriculas para ir colocandolas en las plazas de nuestro parking en
+        * funcion de un numero aleatorio*/
         for (int i = 0; i < matriculas.size(); i++) {
             int numRandom=(int) (Math.random()*2)+1;
             if (numRandom==1){
@@ -93,11 +87,12 @@ public class Parking {
         }
     }
 
-    /**Este metodo se encarga de introducir coches en plazas para no discapacitados
-     * Si se supera el 85% saltara una excepción
-     * Si se llena el parking saltara una excepción
-     * Si un coche vuleve a entrar y ya esta aparcado saltara una excepción*/
+    /**Este metodo se encarga de introducir coches en plazas para no discapacitados.
+     * Si se supera el 85% saltara una excepción.
+     * Si se llena el parking saltara una excepción.
+     * Si un coche vuleve a entrar y ya esta aparcado saltara una excepción.*/
     public int entraCotxe(String matricula) throws Exception{
+        //Lo primero que hacemos es comprobar que la matricula introducida es correcta.
         if (matricula.length()==7){
             char[] chars= matricula.toCharArray();
             for (int i = 0; i < chars.length-3; i++) {
@@ -115,20 +110,34 @@ public class Parking {
         }else {
             throw new Exception("ALERTA =====> Matrícula incorrecte.");
         }
-        for (int i = 0; i < plazas.length; i++) {
-            if (plazas[i].equals(matricula)){
-                throw new Exception("El cotxe ja està al parking. No pot entrar.");
-            }else if(plazas[i]!=""){
-                if (capacidad(TipusPlacesParking.No_Discapacitat)){
-                    throw new Exception("ALERTA =====> Parking per no discapacitats ple.");
+        //Comprobamos si el parking esta lleno, o no.
+        if (capacidad(TipusPlacesParking.No_Discapacitat)){
+            throw new Exception("ALERTA =====> Parking per no discapacitats ple.");
+        }else {
+            for (int i = 0; i < plazas.length; i++) {
+                //Lo sigiente es comprobar si el coche ya esta en el parking.
+                if (plazas[i].equals(matricula)){
+                    throw new Exception("El cotxe ja està al parking. No pot entrar.");
+                }else if (plazas[i]==""){ //Si la posición esta vacia introducimos la matricula
+                    //Comprobamos si la matricula esta como un discapacitadado
+                    for (int j = 0; j < plazasD.length; j++) {
+                        if (plazasD[j].equals(matricula)){
+                            throw new Exception("El cotxe ja està al parking. No pot entrar.");
+                        }
+                    }
+                    plazas[i]=matricula;
+                    plazasOcupadas++;
+                    //Tras asignar la posición generamos un numero aleatorio para meter un garrulo de forma aleatoria en el parking de discapacitados.
+                    int garrulo=(int)((Math.random()*99)+1);
+                    if (garrulo<=10){
+                        entraCotxeDiscapacitat(matricula);
+                    }
+                    //Tras asignar la posición comprobamos si se ha llegado al 85% de capacidad.
+                    if (porcentCapacidad(TipusPlacesParking.No_Discapacitat)){
+                        throw new Exception("ALERTA =====> Ocupació de places per discapacitats supera el 85%.");
+                    }
+                    return i;
                 }
-            }else{
-                plazas[i]=matricula;
-                plazasOcupadas++;
-                if (porcentCapacidad(TipusPlacesParking.No_Discapacitat)){
-                    throw new Exception("ALERTA =====> Ocupació de places per discapacitats supera el 85%.");
-                }
-                return i;
             }
         }
         return -1;
@@ -140,6 +149,7 @@ public class Parking {
      * Si un coche vuleve a entrar y ya esta aparcado saltara una excepción
      * Si un coche de no discapacitado entra en una plaza saltara una excepción*/
     public int entraCotxeDiscapacitat(String matricula) throws Exception {
+        //Comprobamos si la matricula es correcta.
         if (matricula.length()==7){
             char[] chars= matricula.toCharArray();
             for (int i = 0; i < chars.length-3; i++) {
@@ -157,31 +167,38 @@ public class Parking {
         }else {
             throw new Exception("ALERTA =====> Matrícula incorrecte.");
         }
-        for (int i = 0; i < plazasD.length; i++) {
-            if (plazasD[i].equals(matricula)){
-                throw new Exception("El cotxe ja està al parking. No pot entrar.");
-            }else if(plazasD[i]!=""){
-                if (capacidad(TipusPlacesParking.Discapacitat)&&!(capacidad(TipusPlacesParking.No_Discapacitat))){
-                    throw new Exception("ALERTA =====> Parking per discapacitats ple. Ha ocupat plaça normal num: "+entraCotxe(matricula));
-                }else if (capacidad(TipusPlacesParking.Discapacitat)){
-                    throw new Exception("ALERTA =====> Parking per discapacitats ple.");
-                }
-            }else {
-                for (int j = 0; j <plazas.length; j++) {
-                    if (plazas[j].equals(matricula)){
-                        plazas[j]="";
-                        plazasOcupadas--;
-                        plazasD[i]=matricula;
-                        plazasOcupadasD++;
-                        throw new Exception("ALERTA =====> Garrulo detected!!! Ha aparcat a la plaça: "+i);
+        //Comprobamos la capacidad del parking.
+        if (capacidad(TipusPlacesParking.Discapacitat)&&!(capacidad(TipusPlacesParking.No_Discapacitat))){
+            throw new Exception("ALERTA =====> Parking per discapacitats ple. Ha ocupat plaça normal num: "+entraCotxe(matricula));
+        }else if (capacidad(TipusPlacesParking.Discapacitat)){
+            throw new Exception("ALERTA =====> Parking per discapacitats ple.");
+        }else {
+            for (int i = 0; i < plazasD.length; i++) {
+                //Comprobamos si la matricula ya esta en el parking.
+                if (plazasD[i].equals(matricula)){
+                    throw new Exception("El cotxe ja està al parking. No pot entrar.");
+                }else if(plazasD[i]==""){
+                    /*Comprobamos si la matricula es de un no Dicapacitado, si es asi lanzamos una excepción.
+                    En caso contrario continua con el programa y le asigna una plaza*/
+                    for (int j = 0; j <plazas.length; j++) {
+                        /*Para saber si es un garrulo previamente lo hemos guardado en el parking de no discapacitados
+                         y si esta es por tanto un garrulo. Lo sacamos del parkig normal y le asignamos uno del de discapacitados*/
+                        if (plazas[j].equals(matricula)){
+                            plazas[j]="";
+                            plazasOcupadas--;
+                            plazasD[i]=matricula;
+                            plazasOcupadasD++;
+                            throw new Exception("ALERTA =====> Garrulo detected!!! Ha aparcat a la plaça: "+i);
+                        }
                     }
+                    plazasD[i]=matricula;
+                    plazasOcupadasD++;
+                    //Comprobamos si llegamos al 85% de la ocupación.
+                    if (porcentCapacidad(TipusPlacesParking.Discapacitat)){
+                        throw new Exception("ALERTA =====> Ocupació de places per discapacitats supera el 85%.");
+                    }
+                    return i;
                 }
-                plazasD[i]=matricula;
-                plazasOcupadasD++;
-                if (porcentCapacidad(TipusPlacesParking.Discapacitat)){
-                    throw new Exception("ALERTA =====> Ocupació de places per discapacitats supera el 85%.");
-                }
-                return i;
             }
         }
         return -1;
@@ -190,7 +207,9 @@ public class Parking {
     /**Este metodo se encarga de sacar los coches de las plazas de parking para no discapacitados
      * Si se intenta sacar un coche que ya no esta en el parking saltara una excepción*/
     public void surtCotxe(String matricula) throws Exception {
+        //Generamos un booleano para controlar si un coche esta en el parking o no.
         boolean bandera=false;
+        //Comprobamos que la matricula es correcta.
         if (matricula.length()==7){
             char[] chars= matricula.toCharArray();
             for (int i = 0; i < chars.length-3; i++) {
@@ -200,7 +219,7 @@ public class Parking {
                 }
             }
             for (int i = 4; i < chars.length; i++) {
-                if (chars[i]>=65&&chars[i]<=90){
+                if (chars[i]<=65&&chars[i]>=90){
                 }else {
                     throw new Exception("ALERTA =====> Matrícula incorrecte.");
                 }
@@ -208,11 +227,13 @@ public class Parking {
         }else {
             throw new Exception("ALERTA =====> Matrícula incorrecte.");
         }
+        //Comprobamos si la matricula esta o no en el parking.
         for (int i = 0; i < plazas.length; i++) {
             if (plazas[i].equals(matricula)){
                 bandera=true;
             }
         }
+        //Si la matricula no esta en el parking lanzamos una excepción, en caso contrario sacamos la matricula del parking.
         for (int i = 0; i < plazas.length; i++) {
             if (plazas[i].equals(matricula)){
                 plazas[i]="";
@@ -226,7 +247,9 @@ public class Parking {
     /**Este metodo se encarga de sacar los coches de las plazas de parking para discapacitados
      * Si se intenta sacar un coche que ya no esta en el parking saltara una excepción*/
     public void surtCotxeDiscapacitat(String matricula) throws Exception{
+        //Generamos el boolean para comprovar la matricula.
         boolean bandera=false;
+        //Comprovamos si esta bien formada la matricula.
         if (matricula.length()==7){
             char[] chars= matricula.toCharArray();
             for (int i = 0; i < chars.length-3; i++) {
@@ -244,11 +267,13 @@ public class Parking {
         }else {
             throw new Exception("ALERTA =====> Matrícula incorrecte.");
         }
+        //Comprovamos si esta en el parking o no.
         for (int i = 0; i < plazasD.length; i++) {
             if (plazasD[i].equals(matricula)){
                 bandera=true;
             }
         }
+        //Si la matricula esta en el parking la sacamos, encaso contrario lanzamos una excepción.
         for (int i = 0; i < plazasD.length; i++) {
             if (plazasD[i].equals(matricula)){
                 plazasD[i]="";
@@ -282,33 +307,24 @@ public class Parking {
         }
     }
 
-    /***/
+    /**Este metodo se encargara de guardar todas las matriculas del parking*/
     public void guardarMatricula(String path) throws  Exception{
-        String txt;
+        //Comprobamos que el path es correcto.
         try {
             bw=new BufferedWriter(new FileWriter(path,true));
         }catch (Exception e){
             throw new Exception("ALERTA =====> Fitxer incorrecte o inexistent.");
         }
-        System.out.print("Matricula: ");
-        txt=sc.nextLine();
-        if (txt.length()==7){
-            char[] chars=txt.toCharArray();
-            for (int i = 0; i < chars.length-3; i++) {
-                if (chars[i]>=48&&chars[i]<=57){
-                }else {
-                    throw new Exception("ALERTA =====> Matrícula incorrecte.");
-                }
+        for (int i = 0; i < plazasD.length; i++) {
+            if (plazasD[i]!=""){
+                bw.write(plazasD[i]+"\n");
             }
-            for (int i = 4; i < chars.length; i++) {
-                if (chars[i]>=65&&chars[i]<=90){
-                }else {
-                    throw new Exception("ALERTA =====> Matrícula incorrecte.");
-                }
-            }        }else {
-            throw new Exception("ALERTA =====> Matrícula incorrecte.");
         }
-        bw.write(txt+"\n");
+        for (int i = 0; i < plazas.length; i++) {
+            if (plazas[i]!=""){
+                bw.write(plazas[i]+"\n");
+            }
+        }
         bw.close();
     }
 
